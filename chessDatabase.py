9a2a70ae29627b7a0b0e9a3c7bf7f3dfaab3cfa1
Johan Bjerkem <bjerkem.j@gmail.com>
2023-02-23 13:15:37 +0100

    updates

--- chessDatabase.py
+++ chessDatabase.py
@@ -1,6 +1,7 @@
 from chessGame import ChessGame
 import os
 import math
+from typing import List
 
 ROOT = os.path.dirname(os.path.abspath(__file__))
 
@@ -9,49 +10,46 @@ class ChessDatabase:
     def __init__(self):
         self.games = []
 
-    def getGames(self) -> list:
+    def getAllGames(self) -> List[ChessGame]:
         return self.games
     
-    def getTotalMoves(self):
-        return [game.getTotalMoves() for game in self.getGames()]
+    def getTotalMovesOfGames(self, games: List[ChessGame]):
+        return [game.getTotalMoves() for game in games]
     
-    def getActiveGames(self, playCount: int) -> list:
-        return [game for game in self.getGames() if game.getTotalMoves() > playCount]
+    def filterByMoves(self, games: List[ChessGame], playCount: int) -> List[ChessGame]:
+        return [game for game in games if game.getTotalMoves() > playCount]
 
-    def getGamesStockfishWon(self, playCount: int = 0) -> int:
-        return sum([game.getStockfishWon() for game in self.getActiveGames(playCount)])
+    def filterStockfishWon(self, games: List[ChessGame]) -> List[ChessGame]:
+        return [game for game in games if game.getStockfishWon()]
 
-    def getGamesDrawn(self, playCount: int = 0) -> int:
-        return sum([game.getDraw() for game in self.getActiveGames(playCount)])
+    def filterDrawn(self, games: List[ChessGame]) -> List[ChessGame]:
+        return [game for game in games if game.getDraw()]
 
-    def getGamesStockfishLost(self, playCount: int = 0) -> int:
-        return sum([game.getStockfishLost() for game in self.getActiveGames(playCount)])
+    def filterStockfishLost(self, games: List[ChessGame]) -> List[ChessGame]:
+        return [game for game in games if game.getStockfishLost()]
 
-    def getGamesStockfishWhiteWin(self, playCount: int = 0) -> int:
-        return sum([game.getStockfishWhiteWin() for game in self.getActiveGames(playCount)])
+    def filterStockfishWhite(self, games: List[ChessGame]) -> List[ChessGame]:
+        return [game for game in games if game.getStockfishWhite()]
 
-    def getGamesStockfishWhiteLoss(self, playCount: int = 0) -> int:
-        return sum([game.getStockfishWhiteLoss() for game in self.getActiveGames(playCount)])
+    def filterStockfishBlack(self, games: List[ChessGame]) -> List[ChessGame]:
+        return [game for game in games if not game.getStockfishWhite()]
 
-    def getGamesStockfishWhiteDraw(self, playCount: int = 0) -> int:
-        return sum([game.getStockfishWhiteDraw() for game in self.getActiveGames(playCount)])
-
-    def getGamesStockfishBlackWin(self, playCount: int = 0) -> int:
-        return sum([game.getStockfishBlackWin() for game in self.getActiveGames(playCount)])
-
-    def getGamesStockfishBlackLoss(self, playCount: int = 0) -> int:
-        return sum([game.getStockfishBlackLoss() for game in self.getActiveGames(playCount)])
-
-    def getGamesStockfishBlackDraw(self, playCount: int = 0) -> int:
-        return sum([game.getStockfishBlackDraw() for game in self.getActiveGames(playCount)])
-
-    def getOngoingList(self):
+    def getNumListOngoingGames(self, games: List[ChessGame]) -> List[int]:
         activeGames = []
         playCount = 0
-        while len(self.getActiveGames(playCount))>0:
-            activeGames.append(len(self.getActiveGames(playCount)))
+        while True:
+            filteredGames = self.filterByMoves(games, playCount)
+            activeGames.append(len(filteredGames))
             playCount+=1
+            if len(filteredGames) == 0:
+                break
         return activeGames
+    
+    def getDataPoints(self, games: List[ChessGame]) -> str:
+        dataPoints = ""
+        for i, game in enumerate(games):
+            dataPoints+= f'({i},{game})'
+        return dataPoints
     
     def getMean(self, games: list):
         return round(sum(games)/len(games),2)
@@ -60,7 +58,7 @@ class ChessDatabase:
         mean = self.getMean(games)
         return round(math.sqrt(sum([abs(game - mean) for game in games])/len(games)), 2)
        
-    def addGame(self, pgn: str) -> None:
+    def _addGame(self, pgn: str) -> None:
         self.games.append(ChessGame(pgn))
 
     def addGames(self, fileName: str = "Stockfish_15_64-bit.commented.[2600].pgn") -> None:
@@ -69,14 +67,34 @@ class ChessDatabase:
             metaDataGameInfoSplit = fileText.split("\n\n")
             for i in range(0, len(metaDataGameInfoSplit), 2):
                 gameInfo = "\n\n".join(metaDataGameInfoSplit[i:i+2])
-                self.addGame(gameInfo)
+                self._addGame(gameInfo)
 
     def createPdf(self):
-        ongoing = self.getOngoingList()
-        dataPointsAllGames= ""
+        gamesAll = self.getAllGames()
+        gamesStockfishWhite = self.filterStockfishWhite(gamesAll)
+        gamesStockfishBlack = self.filterStockfishBlack(gamesAll)
 
-        for i, game in enumerate(ongoing):
-            dataPointsAllGames+= f'({i},{game})'
+        gamesStockfishWon = self.filterStockfishWon(gamesAll)
+        gamesStockfishDrawn = self.filterDrawn(gamesAll)
+        gamesStockfishLost = self.filterStockfishLost(gamesAll)
+
+        gamesWhiteStockfishWon = self.filterStockfishWhite(gamesStockfishWon)
+        gamesWhiteStockfishDrawn = self.filterStockfishWhite(gamesStockfishDrawn)
+        gamesWhiteStockfishLost = self.filterStockfishWhite(gamesStockfishLost)
+
+        gamesBlackStockfishWon = self.filterStockfishBlack(gamesStockfishWon)
+        gamesBlackStockfishDrawn = self.filterStockfishBlack(gamesStockfishDrawn)
+        gamesBlackStockfishLost = self.filterStockfishBlack(gamesStockfishLost)
+
+        onGoing = self.getNumListOngoingGames(gamesAll)
+        onGoingWhite = self.getNumListOngoingGames(gamesStockfishWhite)
+        onGoingBlack = self.getNumListOngoingGames(gamesStockfishBlack)
+
+        dataPointsAllGames = self.getDataPoints(onGoing)
+        dataPointsStockFishWhiteGames = self.getDataPoints(onGoingWhite)
+        dataPointsStockFishBlackGames = self.getDataPoints(onGoingBlack)
+
+        
                 
         with open('sometexfile.tex', 'w') as file:
             file.write('\\documentclass{article}\n')
@@ -95,9 +113,9 @@ class ChessDatabase:
             file.write('\\begin{tabular}{l|rrr}\n')
             file.write('Color/Result & Wins & Draws & Losses\\\\\n')
             file.write('\hline\n')
-            file.write(f'Any & {self.getGamesStockfishWon()} & {self.getGamesDrawn()} & {self.getGamesStockfishLost()}\\\\\n')
-            file.write(f'White & {self.getGamesStockfishWhiteWin()} & {self.getGamesStockfishWhiteDraw()} & {self.getGamesStockfishWhiteLoss()}\\\\\n')
-            file.write(f'Black & {self.getGamesStockfishBlackWin()} & {self.getGamesStockfishBlackDraw()} & {self.getGamesStockfishBlackLoss()}\\\\\n')
+            file.write(f'Any & {len(gamesStockfishWon)} & {len(gamesStockfishDrawn)} & {len(gamesStockfishLost)}\\\\\n')
+            file.write(f'White & {len(gamesWhiteStockfishWon)} & {len(gamesWhiteStockfishDrawn)} & {len(gamesWhiteStockfishLost)}\\\\\n')
+            file.write(f'Black & {len(gamesBlackStockfishWon)} & {len(gamesBlackStockfishDrawn)} & {len(gamesBlackStockfishLost)}\\\\\n')
             file.write('\end{tabular}\n')
             file.write('\end{center}\n\n')
 
@@ -130,7 +148,7 @@ class ChessDatabase:
 
             file.write('\\end{axis}\n')
             file.write('\\end{tikzpicture}\n')
-            file.write('\caption{Mean moves: ' + str(self.getMean(self.getTotalMoves())) + ', SD: ' + str(self.getSd(self.getTotalMoves())) + '}\n')
+            file.write('\caption{Mean moves: ' + str(self.getMean(onGoing)) + ', SD: ' + str(self.getSd(onGoing)) + '}\n')
             file.write('\end{figure}\n')
 
             file.write("Below is a graph showing the number of games that is still active after $x$ number of moves.\n\n")
@@ -139,9 +157,9 @@ class ChessDatabase:
             file.write('\\begin {tikzpicture}\n')
             file.write('\\begin{axis}[\n')
             file.write('\txmin = 0, xmax = 425,\n')
-            file.write('\tymin = 0, ymax = 2700,\n')
+            file.write('\tymin = 0, ymax = 1350,\n')
             file.write('\txtick distance = 50,\n')
-            file.write('\tytick distance = 300,\n')
+            file.write('\tytick distance = 150,\n')
             file.write('\tgrid = both,\n')
             file.write('\tminor tick num = 1,\n')
             file.write('\tmajor grid style = {lightgray},\n')
@@ -154,15 +172,28 @@ class ChessDatabase:
             file.write('\\addplot[\n')
             file.write('\tsmooth,\n')
             file.write('\tthick,\n')
-            file.write('\tred,\n')
+            file.write('\tblue,\n')
             file.write(']\n')
             file.write('coordinates {\n')
-            file.write(dataPointsAllGames + "\n")
+            file.write(dataPointsStockFishWhiteGames + "\n")
             file.write('};\n')
+            file.write('\\addlegendentry{Stockfish white}\n')
+
+            file.write('\\addplot[\n')
+            file.write('\tsmooth,\n')
+            file.write('\tthick,\n')
+            file.write('\tblack,\n')
+            file.write(']\n')
+            file.write('coordinates {\n')
+            file.write(dataPointsStockFishBlackGames + "\n")
+            file.write('};\n')
+            file.write('\\addlegendentry{Stockfish Black}\n')
+
+            file.write('\include{dice.pgn}\n')
 
             file.write('\\end{axis}\n')
             file.write('\\end{tikzpicture}\n')
-            file.write('\caption{Mean moves: ' + str(self.getMean(self.getTotalMoves())) + ', SD: ' + str(self.getSd(self.getTotalMoves())) + '}\n')
+            # file.write('\caption{Mean moves: ' + str(self.getMean(self.getTotalMoves())) + ', SD: ' + str(self.getSd(self.getTotalMoves())) + '}\n')
             file.write('\end{figure}\n')
 
             file.write('\\end{document}\n')
@@ -178,8 +209,7 @@ def main():
     db.addGames()
     print()
     db.createPdf()
-    print(db.getMean(db.getTotalMoves()))
-    print(db.getSd(db.getTotalMoves()))
+
 
 
 
